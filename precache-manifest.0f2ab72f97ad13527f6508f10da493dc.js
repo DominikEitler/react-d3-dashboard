cfc0400500887150468dcd558a8bd358b064769b
Dominik Eitler <eitler.domi@gmail.com>
2021-01-15 20:13:36 +0100

    Updates

---
+++ precache-manifest.0f2ab72f97ad13527f6508f10da493dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abce00293038cc6861748534ade5eb2a",
+    "url": "/react-d3-dashboard/index.html"
+  },
+  {
+    "revision": "540ad65c4561189f3021",
+    "url": "/react-d3-dashboard/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "82e8317ebbf99f84d730",
+    "url": "/react-d3-dashboard/static/css/main.57abb245.chunk.css"
+  },
+  {
+    "revision": "540ad65c4561189f3021",
+    "url": "/react-d3-dashboard/static/js/2.1f0ae3b1.chunk.js"
+  },
+  {
+    "revision": "82e8317ebbf99f84d730",
+    "url": "/react-d3-dashboard/static/js/main.ead13864.chunk.js"
+  },
+  {
+    "revision": "a09e4a025c08d48407b8",
+    "url": "/react-d3-dashboard/static/js/runtime~main.a573e2e4.js"
+  },
+  {
+    "revision": "46a9b0988c83b02d27640bf9ced3ab95",
+    "url": "/react-d3-dashboard/static/media/adult.46a9b098.csv"
+  }
+]);
